378a97e2e795405a918a51eabfd1c16094309155
tvenema <t.h.venema@hotmail.com>
2021-05-02 08:52:20 +0200

    Tjerks aanpassingen

--- experiment_cp_parameter.py
+++ experiment_cp_parameter.py
@@ -3,30 +3,37 @@ from matplotlib import pyplot as plt
 import numpy as np
 from target_structures import target_structure_dict
 
-def cp_experiments(structures, cp_parameters=[10**i for i in range(-3,4)], plot=True, evaluations=60):
+def cp_experiments(structures, cp_parameters=[10**i for i in range(-3,4)], plot=True, evaluations=750):
     """runs for given structures the Cp parameter (x) against similarity (y)"""
-    averages_vs_cp = []
-    std_vs_cp = []
+    mean_similarities = np.empty(0)
+    std_similarities = np.empty(0)
     for cp_parameter in cp_parameters:
-        similarities = []
-        for structure in structures:
-            similarities.append(MonteCarloTreeSearch(structure, cp_parameter, max_evaluations=evaluations).run()[1])
-        averages_vs_cp.append(np.mean(similarities))
-        std_vs_cp.append(np.std(similarities))
+        similarities = np.empty(0)
+        for structure, i in zip(structures, range(len(structures))):
+            for j in range(1):
+                similarity = MonteCarloTreeSearch(structure, RNA = False, c_parameter = cp_parameter, max_evaluations=evaluations).run()[1]
+                print(f'Cp = {cp_parameter}, i = {i}, j = {j}, sim = {similarity:.2f}')
+                similarities = np.append(similarities, similarity)
+        mean_similarities = np.append(mean_similarities, np.mean(similarities))
+        std_similarities = np.append(std_similarities, np.std(similarities))
 
     if plot:
         #improve plot: plot from 0.5 (random) to 1 and logarthmic scale on x acix.
-        plt.errorbar(x=cp_parameters, y=averages_vs_cp, yerr=std_vs_cp, c="black", capsize=10)
+        color = 'g'
+        plt.plot(cp_parameters, mean_similarities, color = color)
+        y_minus_error = mean_similarities - std_similarities
+        y_plus_error = mean_similarities + std_similarities
+        plt.fill_between(cp_parameters, y_minus_error, y_plus_error, color = color, alpha = 0.2)
         plt.xscale('log')
         plt.ylim(0.5, 1)
-        plt.xlabel("Cp parameter")
+        plt.xlabel(r"$C_p$")
         plt.ylabel("Similarity")
-        plt.title("Sequence length: "+ str(len(structure)) + ", function evaluations: 36")
-        plt.savefig("cp_plot")
+        plt.title(f"Sequence length: {len(structure)}, max MCTS evaluations: {evaluations}")
+        plt.savefig(f"cp_plot_{evaluations}.pdf")
         plt.show()
-        None
-
 
 if __name__ == "__main__":
     length = 180
     cp_experiments(target_structure_dict[length], cp_parameters=[10**i for i in range(-3,4)])
+    # cp_experiments(target_structure_dict[length], cp_parameters=[1,10])
+    
